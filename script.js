7ef741261957382295172e81a6b65344b1e2ff40
SerzhykAspirant <156260445+SerzhykAspirant@users.noreply.github.com>
2024-02-13 04:03:24 +0200

    Update

--- script.js
+++ script.js
@@ -3,35 +3,23 @@ document.addEventListener("DOMContentLoaded", () => {
     const card = document.querySelector(".card");
     const audio = document.getElementById('myAudio');
 
-    const raiseCard = () => {
+    const activateCard = () => {
+        // Піднімає картку
         card.style.transition = "top 0.5s";
         card.style.top = "-90px";
-    };
 
-    const lowerCard = () => {
-        card.style.transition = "top 0.5s";
-        card.style.top = "0";
-    };
-
-    envelope.addEventListener("mouseenter", raiseCard);
-    envelope.addEventListener("mouseleave", lowerCard);
-
-    envelope.addEventListener("click", (e) => {
-        e.stopPropagation(); // Запобігає розповсюдженню події на батьківські елементи
-        raiseCard();
-
+        // Вмикає музику
         if (audio.paused) {
             audio.play();
         } else {
-            audio.currentTime = 0;
+            audio.currentTime = 0; // Якщо вам потрібно починати відтворення з початку кожного разу
             audio.play();
         }
-    });
+    };
 
-    // Додатковий обробник для опускання картки тільки якщо клік відбувся поза конвертом
-    document.addEventListener("click", (e) => {
-        if (!envelope.contains(e.target)) {
-            lowerCard();
-        }
-    }, true);
+    envelope.addEventListener("mouseenter", activateCard);
+    envelope.addEventListener("click", (e) => {
+        e.stopPropagation(); // Запобігає розповсюдженню події на батьківські елементи
+        activateCard();
+    });
 });
