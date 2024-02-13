818f8a629c19605436634c4bfa0c6c32158d6a21
SerzhykAspirant <156260445+SerzhykAspirant@users.noreply.github.com>
2024-02-13 03:48:11 +0200

    Update

--- script.js
+++ script.js
@@ -35,3 +35,30 @@ document.addEventListener('DOMContentLoaded', (event) => {
             audio.currentTime = 0;
         }
     });
+    document.addEventListener("DOMContentLoaded", () => {
+        const container = document.querySelector(".container");
+        const card = document.querySelector(".card");
+    
+        // Функція для підняття листа
+        const raiseCard = () => {
+            card.style.transition = "top 0.5s";
+            card.style.top = "-90px"; // Відрегулюйте це значення за потреби
+        };
+    
+        // Функція для опускання листа
+        const lowerCard = () => {
+            card.style.transition = "top 0.5s";
+            card.style.top = "0px"; // Відрегулюйте це значення за потреби
+        };
+    
+        container.addEventListener("mouseenter", raiseCard);
+        container.addEventListener("mouseleave", lowerCard);
+    
+        // Додайте обробник події кліку до container
+        container.addEventListener("click", raiseCard);
+    
+        // Якщо вам потрібно опустити лист при кліку на інший елемент,
+        // вам потрібно додати обробник події кліку до цього елемента. 
+        // Наприклад, клік за межами container:
+        // document.addEventListener("click", lowerCard);
+    });
